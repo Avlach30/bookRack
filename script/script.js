a1fce8f14a2ecb966e22820afed083cc0623c2a9
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-08 13:11:30 +0800

    develop logic in javascript file for rendering data

--- script/script.js
+++ script/script.js
@@ -36,7 +36,7 @@ const loadData = () => {
     }
   }
 
-  // document.dispatchEvent(new Event(RENDER_EVENT));
+  document.dispatchEvent(new Event(RENDER_EVENT));
 };
 
 const createNewBook = () => {
@@ -49,8 +49,68 @@ const createNewBook = () => {
   const book = generateBookObj(id, title, author, year, description, false);
   books.push(book);
 
-  // document.dispatchEvent(new Event(RENDER_EVENT));
+  document.dispatchEvent(new Event(RENDER_EVENT));
   saveData();
+};
+
+const renderBookItem = (book) => {
+  const table = document.createElement('table');
+
+  const titleRowData = document.createElement('td');
+  const title = document.createElement('h3');
+  title.innerText = book.title;
+  titleRowData.append(title);
+  table.append(titleRowData);
+
+  const yearRowData = document.createElement('td');
+  const year = document.createElement('h3');
+  year.innerText = book.year;
+  yearRowData.append(year);
+  table.appendChild(yearRowData);
+
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(table);
+  container.setAttribute('id', `book-${book.id}`);
+
+
+  if (book.isFinishedRead) {
+    const btnActionRowData = document.createElement('td');
+
+    const detailBtn = document.createElement('button');
+    detailBtn.innerText = 'details';
+    detailBtn.classList.add('action-btn', 'detail-button');
+
+    const undoBtn = document.createElement('button');
+    undoBtn.innerText = 'undo';
+    undoBtn.classList.add('action-btn', 'undo-button');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'delete';
+    deleteBtn.classList.add('action-btn', 'delete-button');
+
+    btnActionRowData.append(detailBtn, undoBtn, deleteBtn);
+    table.append(btnActionRowData);
+  } else {
+    const btnActionRowData = document.createElement('td');
+
+    const detailBtn = document.createElement('button');
+    detailBtn.innerText = 'details';
+    detailBtn.classList.add('action-btn', 'detail-button');
+
+    const updateBtn = document.createElement('button');
+    updateBtn.innerText = 'update';
+    updateBtn.classList.add('action-btn', 'update-button');
+
+    const finishBtn = document.createElement('button');
+    finishBtn.innerText = 'finish';
+    finishBtn.classList.add('action-btn', 'finish-button');
+
+    btnActionRowData.append(detailBtn, updateBtn, finishBtn);
+    table.append(btnActionRowData);
+  }
+
+  return container;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -66,6 +126,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (checkStorageExist()) loadData();
 
+});
+
+document.addEventListener(RENDER_EVENT, () => {
+  const unfinishedReadBooks = document.getElementById('books');
+  unfinishedReadBooks.innerHTML = '';
+
+  const finishedReadBooks = document.getElementById('finishedReads');
+  finishedReadBooks.innerHTML = '';
+
+  for (let book of books) {
+    const bookItemElement = renderBookItem(book);
+
+    !bookItemElement.isFinishedRead ? unfinishedReadBooks.append(bookItemElement) : finishedReadBooks.append(bookItemElement);
+  }
 });
 
 document.addEventListener(SAVED_EVENT, () => console.log(localStorage.getItem(LOCAL_STORAGE_KEY)));
